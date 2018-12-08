ebcc689976a41070d8a076d2824aa581d27ef21b
Ashlyn Lee <ashlynlee13@gmail.com>
2018-12-08 14:47:56 -0600

    Rewrite day 5 part 1 with regex

--- src/solutions/day5.js
+++ src/solutions/day5.js
@@ -1,5 +1,9 @@
 import renderOutput from "../utils/render";
 
+const hasAtLeastThreeVowelsPattern = /[a-z]*[aeiou]{1}[a-z]*[aeiou]{1}[a-z]*[aeiou]{1}[a-z]*/;
+const repeatedLetterPattern = /[a-z]*([a-z])\1[a-z]*/;
+const containsInvalidSubstringPattern = /[a-z]*(ab|cd|pq|xy)[a-z]*/;
+
 const repeatedPairPattern = /[a-z]*([a-z][a-z])[a-z]*\1[a-z]*/;
 const repeatWithOneBetweenPattern = /[a-z]*([a-z])[a-z]\1[a-z]*/;
 
@@ -14,33 +18,15 @@ const solve = (input) => {
 };
 
 const isNiceForPart1 = (word) => {
-  return hasAtLeastThreeVowels(word) &&
-         hasRepeatedLetter(word) &&
-         doesNotHaveInvalidString(word);
+  return hasAtLeastThreeVowelsPattern.test(word) &&
+         repeatedLetterPattern.test(word) &&
+         !containsInvalidSubstringPattern.test(word);
 };
 
 const isNiceForPart2 = (word) => {
   return repeatedPairPattern.test(word) &&
     repeatWithOneBetweenPattern.test(word);
 }
-
-const hasAtLeastThreeVowels = (word) => {
-  return word.split('').filter(letter => "aeiou".indexOf(letter) !== -1)
-    .length >= 3;
-};
-
-const hasRepeatedLetter = (word) => {
-  return word.split('').reduce((hasDuplicate, currentLetter, i, word) => {
-    if (i === 0) return false;
-    const previousLetter = word[i - 1];
-    return hasDuplicate || currentLetter === previousLetter;
-  }, false);
-};
-
-const doesNotHaveInvalidString = (word) => {
-  const invalidSubstrings = ["ab", "cd", "pq", "xy"];
-  return invalidSubstrings.every(i => word.indexOf(i) === -1);
-};
 
 export default (input) => {
   const solutions = solve(input);
